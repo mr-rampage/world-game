b29cef2c49e9453dbd2f9c28d647c61b40bbd522
mr-rampage <readonly@che.wbac.ca>
2019-01-29 22:14:50 -0500

    refactor(destinations) rename for domain actor

--- src/villain.actor.js
+++ src/villain.actor.js
@@ -1,13 +1,18 @@
-self.addEventListener('message', function() {
-  $world = fetch('https://restcountries.eu/rest/v2/all')
+self.addEventListener('message', function(e) {
+  switch(e.data) {
+    case 'flee': return flee().then(self.postMessage);
+  }
+}, false);
+
+function flee() {
+  return fetch('https://restcountries.eu/rest/v2/all')
     .then(response => response.json())
     .then(destinations => [
       destinations[randomInt(destinations.length)],
       destinations[randomInt(destinations.length)],
       destinations[randomInt(destinations.length)]
-    ])
-    .then(self.postMessage);
-}, false);
+    ]);
+}
 
 function randomInt(max) {
   return (Math.random() * max) | 0;
