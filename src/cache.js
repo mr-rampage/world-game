cb5cfb3e1a8c5183d9f4c8ff19c999e3d778078b
mr-rampage <readonly@che.wbac.ca>
2019-01-31 23:23:54 -0500

    refactor(cache) clean up

--- src/cache.js
+++ src/cache.js
@@ -5,20 +5,14 @@ self.addEventListener('install', event =>
 self.addEventListener('fetch', event =>
   event.respondWith(caches.match(event.request).then(findResponse(caches, event.request))));
 
-function cacheRequest(caches, request) {
-  return fetch(request)
-    .then(cacheResponse(caches, request));
-}
-
-function cacheResponse(caches, request) {
-  return response => {
-    const cloned = response.clone();
-    caches.open('world-game')
-      .then(cache => cache.put(request, cloned));
-    return response;
-  };
-}
-
 function findResponse(caches, request) {
   return response => response || cacheRequest(caches, request);
+}
+
+async function cacheRequest(caches, request) {
+  const response = await fetch(request);
+  const cloned = response.clone();
+  const cache = await caches.open('world-game');
+  cache.put(request, cloned);
+  return response;
 }
