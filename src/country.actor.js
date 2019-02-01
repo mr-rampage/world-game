442c58606b15092ade5ef2a1e5ca53b54b349db1
mr-rampage <readonly@che.wbac.ca>
2019-01-31 23:59:03 -0500

    refactor(country) clean up

--- src/country.actor.js
+++ src/country.actor.js
@@ -1,25 +1,26 @@
 importScripts('worker-utils.js');
 self.addEventListener('message', messageHandler);
 
-function messageHandler(e) {
+async function messageHandler(e) {
   const {command, message} = e.data;
   switch (command) {
     case 'open-channel':
-      openChannel(message, messageHandler);
+      openChannel(message, messageHandler)
+        .postMessage({command: 'log', message: 'Greetings from Country Actor'});
       break;
     case 'log':
-      log(message);
+      log('Country', message);
       break;
     case 'describe-country':
-      describeCountry(message).then(self.postMessage);
+      self.postMessage(await describeCountry(message), undefined);
       break;
     default:
       break;
   }
 }
 
-function describeCountry(country) {
-  return fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${country}&format=json&origin=*&prop=extracts&explaintext&exintro&redirects=1`)
-    .then(response => response.json())
-    .then(result => result.query.pages[Object.keys(result.query.pages)[0]].extract);
+async function describeCountry(country) {
+  const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${country}&format=json&origin=*&prop=extracts&explaintext&exintro&redirects=1`);
+  const article = await response.json();
+  return article.query.pages[Object.keys(article.query.pages)[0]].extract;
 }
